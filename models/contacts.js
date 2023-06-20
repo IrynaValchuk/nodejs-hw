95ab079dd709446448808fdbe06d76a0f92de953
Iryna <irula17ooo@gmail.com>
2023-06-20 23:24:49 +0300

    add function

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,75 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contactById = contacts.find((contact) => contact.id === contactId);
+    return contactById || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const removedContact = await getContactById(contactId);
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await updateContacts(filteredContacts);
+    return removedContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      ...body,
+    };
+    contacts.push(newContact);
+    await updateContacts(contacts);
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = { id: contactId, ...body };
+    await updateContacts(contacts);
+    return contacts[index];
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +77,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
